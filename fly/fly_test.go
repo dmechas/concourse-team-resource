294d754905673813ba9c45e65a23a4ce1e3251bb
Duarte Mechas <dmechas@equalexperts.com>
2019-12-10 15:29:56 +0000

    Test fly SetTeam

--- fly/fly_test.go
+++ fly/fly_test.go
@@ -16,11 +16,13 @@ var (
 	target          string
 	flyBinaryPath   string
 	teamName        string
+	localUser       string
+	githubTeam      string
 	tempDir         string
 	fakeFlyContents string
 )
 
-func TestSetTeam(t *testing.T) {
+func TestGetTeam(t *testing.T) {
 	target = "some-target"
 	fakeLogger := logger.NewLogger(os.Stderr)
 	teamName = "some-team"
@@ -42,5 +44,38 @@ func TestSetTeam(t *testing.T) {
 
 	expected := fmt.Sprintf("%s %s %s %s %s",
 		"-t", target, "get-team", "-n", teamName)
+	assert.Equal(t, expected, string(output))
+}
+
+func TestSetTeam(t *testing.T) {
+	target = "some-target"
+	fakeLogger := logger.NewLogger(os.Stderr)
+	teamName = "some-team"
+	localUser = "some-user"
+	githubTeam = "some-team"
+	var err error
+	tempDir, err = ioutil.TempDir("", "")
+	assert.NoError(t, err)
+
+	flyBinaryPath = filepath.Join(tempDir, "fake_fly")
+	fakeFlyContents = `#!/bin/bash
+	echo -n "$@"`
+
+	err = ioutil.WriteFile(flyBinaryPath, []byte(fakeFlyContents), os.ModePerm)
+	assert.NoError(t, err)
+
+	flyCommand := fly.NewCommand(target, fakeLogger, flyBinaryPath)
+
+	output, err := flyCommand.SetTeam(teamName, localUser, githubTeam)
+	assert.NoError(t, err)
+
+	expected := fmt.Sprintf("%s %s %s %s %s %s %s=%s %s=%s",
+		"-t", target,
+		"set-team",
+		"--non-interactive",
+		"-n", teamName,
+		"--local-user", localUser,
+		"--github-team", githubTeam,
+	)
 	assert.Equal(t, expected, string(output))
 }
